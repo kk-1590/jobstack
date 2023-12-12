407d872be13d822ab3bb4db95d4a50cb5a13d46d
kk-1590 <kkhandelwal1590@gmail.com>
2023-12-13 01:34:41 +0530

    admin page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,7 @@ import {loader as allJobsLoader} from './pages/AllJobs';
 import {loader as editJobLoader} from './pages/EditJob';
 import {action as editJobAction} from './pages/EditJob';
 import {action as deleteJobAction} from './pages/DeleteJob';
+import {loader as adminLoader} from './pages/Admin';
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -79,6 +80,7 @@ const router = createBrowserRouter([
           {
             path : 'admin',
             element : <Admin />,
+            loader : adminLoader,
           },
           {
             path : 'edit-job/:id',
--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -11,6 +11,10 @@ const NavLinks = ({isBigSidebar}) => {
         <div className="nav-links">
             {links.map((link) => {
                 const {text,path,icon} = link;
+                const {role} = user
+                if(path === 'admin' && role !== 'admin'){
+                    return ;
+                }
                 return (
                     <NavLink to={path}
                     key = {text}
