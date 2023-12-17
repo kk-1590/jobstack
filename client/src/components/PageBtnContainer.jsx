43cdc2ca30bf9bc7af9071005b8a5834cd0d44f6
kk-1590 <kkhandelwal1590@gmail.com>
2023-12-17 22:15:47 +0530

    setup simple pagination

---
+++ client/src/components/PageBtnContainer.jsx
@@ -0,0 +1,68 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useAllJobsContext } from "../pages/AllJobs";
+
+const PageBtnContainer = () => {
+  const {
+    data: { numOfPages, currentPage },
+  } = useAllJobsContext();
+  console.log(numOfPages, currentPage);
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
+  console.log(pages);
+
+  const {search,pathname} = useLocation()
+  const navigate = useNavigate();
+  console.log(search,pathname);
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    console.log(searchParams);
+    searchParams.set('page',pageNumber);
+    console.log(pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <Wrapper>
+      <button className="btn prev-btn" onClick={() => {
+        let prevPage = currentPage - 1;
+        if(prevPage < 1){
+            prevPage = numOfPages;
+        }
+        handlePageChange(prevPage)
+      }}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-conteiner">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              className={`btn page-btn ${
+                pageNumber === currentPage && "active"
+              }`}            
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className="btn next-btn" onClick={() => {
+        let nextPage = currentPage + 1;
+        if(nextPage > numOfPages){
+            nextPage = 1;
+        }
+        handlePageChange(nextPage);
+      }}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
+};
+
+export default PageBtnContainer;
